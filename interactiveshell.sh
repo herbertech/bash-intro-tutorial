6dcc34a2697e39ea212a8be60ae5ab72463e0624
Herbertech <contact@herbertech.be>
2022-11-12 09:21:48 +0000

    first commit

---
+++ interactiveshell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "What is your first name?"
+read FIRST_NAME
+echo "What is your last name?"
+read LAST_NAME
+
+echo "Hello $FIRST_NAME $LAST_NAME"
---
+++ shelltest.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+FIRST_NAME=Herbert
+LAST_NAME=Lindemans
+echo $FIRST_NAME $LAST_NAME
